17bdd5ffe8e5a9e3a3ba6fb62b5bc91f15d81139
joshbam-joshwilly <j.alana@alustudent.com>
2024-09-13 01:34:54 +0200

    lists

---
+++ data_types/list.dart
@@ -0,0 +1,11 @@
+void main()
+{
+List<String> names = ["Joshua", "Ronald", "Justice"];
+print("Values of names is $names");
+print("Value of names[0] is ${names[0]}");
+print("Value of names[1] is ${names[1]}");
+print("Values of names[2] is ${names[2]}");
+
+int length = names.length;
+print("The Length of names is $length");
+}
