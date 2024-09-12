62bd9e13e1ed3c8fd8e718b7f2ee850135ccd848
joshbam-joshwilly <j.alana@alustudent.com>
2024-09-12 09:19:31 +0200

    some codes for variabes

---
+++ variable/test.dart
@@ -0,0 +1,14 @@
+void main()
+{
+String name = "Joshua Alana!!";
+String address = "KIGALI";
+num age = 20;
+num height = 5.6;
+bool isMarried = false;
+
+print("Name is $name");
+print("Address is $address");
+print("Age is $age");
+print("Height is $height");
+print("Married status is $isMarried");
+}
