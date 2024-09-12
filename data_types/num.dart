c8b2b596acf3466bf76d6df0ec8ebd03e2e0c472
joshbam-joshwilly <j.alana@alustudent.com>
2024-09-13 01:18:52 +0200

    strings in dart

---
+++ data_types/num.dart
@@ -0,0 +1,14 @@
+void main(){
+int num1 = 100;
+double num2 = 130.5;
+num num3 = 50;
+num num4 = 34.6;
+
+
+num sum = num1 + num2 + num3 + num4;
+print("Num 1 is $num1");
+print("Num 2 is $num2");
+print("NUM 3 is $num3");
+print("NUm 4 is $num4");
+print("Sum is $sum");
+}
---
+++ data_types/string.dart
@@ -0,0 +1,5 @@
+void main(){
+	String schoolName = "Diamond School";
+	String address = "New York 5465";
+print("School name is $schoolName and \n address is $address");
+}
