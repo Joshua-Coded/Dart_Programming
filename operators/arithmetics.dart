ee3fd797404baf72371b002a5bcc8010aaa71db8
joshbam-joshwilly <j.alana@alustudent.com>
2024-09-13 02:22:11 +0200

    operators

---
+++ operators/arithmetics.dart
@@ -0,0 +1,20 @@
+ void main(){
+int num1 = 10;
+int num2 = 30;
+
+int sum = num1 + num2;
+int diff = num1 - num2;
+int unaryMinus = -num1;
+int mul = num1 * num2;
+double div = num1 / num2;
+int div2 = num1 ~/ num2;
+int mod = num1 % num2;
+
+
+print("The addition is $sum.");
+print("The substraction is $diff");
+print("The multiplication is $mul.");
+print("The division is $div.");
+print("The integer division is $div2.");
+print("The modulus is $mod");
+}
