d9208531a75df6148f46c00db55d429f8a10a491
joshbam-joshwilly <j.alana@alustudent.com>
2024-09-14 16:35:29 +0200

    task 4

---
+++ week_1_problems/task4.dart
@@ -0,0 +1,10 @@
+// Write a program in Dart that finds simple interest. Formula = (p * t * r) / 100
+void main()
+{
+double p = 1000;
+double t = 5;
+double r = 10;
+
+double simpleInterest = (p * t * r) / 100;
+print("Simple Interest: $simpleInterest");
+}
