1df757f3ca8f2f4ae3066f1945fe6676d0869533
joshbam-joshwilly <j.alana@alustudent.com>
2024-09-14 17:09:50 +0200

    task 11

---
+++ week_1_problems/task11.dart
@@ -0,0 +1,10 @@
+// Write a program to calculate the split amount of a bill. Formula = (total bill amount) / number of people
+
+void main(){
+double totalBill = 2500;
+int numberOfPeople = 4;
+
+double billPerPerson = totalBill / numberOfPeople;
+print("Each person pays: $billPerPerson");
+
+}
