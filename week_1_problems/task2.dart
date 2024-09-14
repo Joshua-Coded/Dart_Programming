99e1811538a33e204f2e295603fec026f729e88b
joshbam-joshwilly <j.alana@alustudent.com>
2024-09-14 12:38:41 +0200

    some codes

---
+++ week_1_problems/task2.dart
@@ -0,0 +1,5 @@
+// Write a program to print "Hello I am 'John Doe'" and "Hello I’m 'John Doe'" using single and double quotes.
+void main(){
+print('Hello I am "Joshua Alana"');
+print("Hello I'm 'Joshua Alana'");
+}
